fe074fedced6ba8c3e6ad1fdbd599c1af9c826f2
Jennifer L Tarleton <63979357+JenniferT9462@users.noreply.github.com>
2024-11-26 20:49:22 -0600

    Completed template-literals

--- template-literals.js
+++ template-literals.js
@@ -32,26 +32,28 @@ console.log(result);
 // Exercises
 
 // 1. Use template literals to create a greeting message that includes a person's name and age
-// const name = "Alice";
-// const age = 30;
+const name2 = "Alice";
+const age = 30;
 // // Your code here
-
+const greeting2 = `Happy Birthday ${name2}! You are ${age} years old!`;
+console.log(greeting2);
 // 2. Create a multi-line string that describes your favorite book, including its title and author
-// const title = "The Catcher in the Rye";
-// const author = "J.D. Salinger";
+const title = "The Catcher in the Rye";
+const author = "J.D. Salinger";
 // // Your code here
-
+console.log(`My favorite book is ${title}, \nBy: ${author}`)
 // 3. Write a template literal that embeds an expression to calculate the area of a rectangle
-// const length = 10;
-// const width = 5;
+const length = 10;
+const width = 5;
 // // Your code here
-
+console.log(`The area of a rectangle with a width of ${width} and length of ${length} is ${length*width}`)
 // 4. Use template literals to create a string that includes a person's first name, last name, and their full name
-// const firstName = "Emma";
-// const lastName = "Brown";
+const firstName = "Emma";
+const lastName = "Brown";
 // // Your code here
-
+console.log(`First Name: ${firstName}, Last Name: ${lastName}, Full Name: ${firstName + ' ' + lastName}`)
 // 5. Write a template literal that uses a conditional expression to output a message based on a score
-// const score = 85;
+const score = 85;
 // // Your code here
-
+const message = `You ${score >= 85 ? 'passed' : 'failed'} the exam.`
+console.log(message)
