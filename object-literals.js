4a1754e613623654c970921a13e47d84cb39c28f
Jennifer L Tarleton <63979357+JenniferT9462@users.noreply.github.com>
2024-11-30 14:23:31 -0600

    Complete object-literals practice

--- object-literals.js
+++ object-literals.js
@@ -36,31 +36,42 @@ console.log("Example 3 - Stats:", stats); // Output: { speed: 100 }
 // Exercises
 
 // 1. Create an object that represents a car with properties 'brand' and 'model' using shorthand property names
-// const brand = 'Toyota';
-// const model = 'Corolla';
-// // Your code here
-
+const brand = 'Toyota';
+const model = 'Corolla';
+ // Your code here
+const car = {brand, model};
+console.log("Exercise 1:", car);
 // 2. Define an object that has a method to calculate the sum of two numbers without using the function keyword
-// const calculator = {
-//     // Your code here
-// };
-
+ const calculator = {
+     // Your code here
+     sum(a, b) {
+        return a + b;
+     }
+ };
+console.log("Exercise 2:", calculator.sum(2,2))
 // 3. Create an object representing a book with a computed property name for its 'rating'
-// const ratingKey = 'rating';
-// const ratingValue = 4.5;
-// // Your code here
-
+ const ratingKey = 'rating';
+ const ratingValue = 4.5;
+ // Your code here
+const book = {
+    [ratingKey]: ratingValue
+}
+console.log("Exercise 3 - Rating:", book);
 // 4. Write an object that represents a student, including a method to introduce themselves
-// const student = {
-//     name: 'Misty',
-//     // Your code here
-// };
-
+ const student = {
+     name: 'Misty',
+     // Your code here
+     greeting() {
+        console.log(`Hi, I'm ${this.name}`)
+     }
+ };
+ student.greeting();
 // 5. Create an object with shorthand properties and a computed property for the release year
-// const title = 'Pokemon Red';
-// const creator = 'Game Freak';
-// const releaseYearKey = 'releaseYear';
-// const releaseYearValue = 1996;
-// // Your code here
-
+ const title = 'Pokemon Red';
+ const creator = 'Game Freak';
+ const releaseYearKey = 'releaseYear';
+ const releaseYearValue = 1996;
+ // Your code here
+const pokemonGame = { title, creator, [releaseYearKey]: releaseYearValue};
+console.log("Exercise 5:", pokemonGame)
 // Solutions should be written directly below each TODO comment.
